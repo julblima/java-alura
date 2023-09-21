a731a4ec300c32500d8f20be0ad79f3b514b7eb6
julblima <130477259+julblima@users.noreply.github.com>
2023-09-21 11:32:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play()
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1
+
+    console.log(contador)
+}
